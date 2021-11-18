7dec863947203a12bfdb60c0d723d4c536d95b8a
proflamyt <53262578+proflamyt@users.noreply.github.com>
2021-11-18 12:45:24 +0000

    Update ok.py

--- ok.py
+++ ok.py
@@ -9,15 +9,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
 
+@app.route('/test')
+def test():
+  return 'ok'
 
 
-
+ola = ["{'state':false, 'component':25}", "{'state':true, 'component':26}", "{'state':false, 'component':27}"]
 async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(message)
+    while True:
+      #  async for message in websocket:
+      for i in ola:
+        await websocket.send(i)
+        await asyncio.sleep(1)
 
 async def main():
-    async with serve(echo, "127.0.0.1", 8765,ping_interval=None):
+    async with serve(echo, "localhost", 8766,ping_interval=None):
         await asyncio.Future()
 
 
